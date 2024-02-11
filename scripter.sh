0a82f24fd2a037fa970d053a0e74854ed315d893
Leaderboard <leader-board@outlook.com>
2024-02-11 11:27:17 +0300

    try printing arguments

--- scripter.sh
+++ scripter.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+echo "$@"
 cd /statdata/Wikimedia-statistics
 for((i=1;i<=$#;i++)); 
-do mysql --defaults-file=$HOME/replica.my.cnf -h ${!i}.analytics.db.svc.wikimedia.cloud ${!i}_p -e "SELECT user_name, user_registration, user_editcount from user ORDER BY user_editcount desc;" > ${!i}.csv; 
+do mysql --defaults-file="$HOME"/replica.my.cnf -h "${!i}".analytics.db.svc.wikimedia.cloud "${!i}"_p -e "SELECT user_name, user_registration, user_editcount from user ORDER BY user_editcount desc;" > "${!i}".csv;
 done
 rm -r ../rawcsv
 mkdir -p ../rawcsv
